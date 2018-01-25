295fea4ae931720674a5c04089025ec9934995c2
Nathan <nathan@rstudio.com>
2018-01-25 09:59:21 +0000

    updating

--- images.R
+++ images.R
@@ -1,13 +1,18 @@
-install.packages(webshot)
-webshot::install_phantomjs()
+#install.packages(webshot)
+#webshot::install_phantomjs()
 
-urls <- c("http://54.149.163.100:3939/content/1/tmp.nb.html",
-           "http://54.149.163.100:3939/content/2/",
-          "http://54.149.163.100:3939/content/4/build-r-from-source.nb.html",
-          "http://54.149.163.100:3939/content/6/install-rstudio.nb.html",
-          "http://54.149.163.100:3939/content/7/install-linux-libraries.nb.html",
-          "http://54.149.163.100:3939/content/8/README.html",
-          "http://54.149.163.100:3939/content/9/install-postgresql.nb.html",
-          "http://54.149.163.100:3939/content/10/__swagger__/")
+urls <- c(
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/13/README.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/12/setup-instance.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/4/build-r-from-source.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/7/install-linux-libraries.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/6/install-rstudio.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/14/install-r-packages.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/9/install-postgresql.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/17/config-github.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/15/install-CMS.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/16/config-email.nb.html",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/11/",
+  "http://ec2-54-149-163-100.us-west-2.compute.amazonaws.com:3939/content/10/__swagger__/")
 
-webshot::webshot(urls, "html/img/thumb.png", vheight = 600, vwidth = 600, cliprect = "viewport")
+webshot::webshot(urls, "html/img/thumb.png", vheight = 600, vwidth = 600, cliprect = "viewport", delay = 2)
